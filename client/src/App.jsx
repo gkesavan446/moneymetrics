887c27689476ec61be8938ebd015d7288d7154ec
gkesavan446 <gkesavan446@gmail.com>
2026-08-20 21:00:26 +0530

    redirect to login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
@@ -18,6 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/"  element={<Navigate to="/login" replace />}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
